5927f216c358d2a3b221d6fe9f57feebe9f6df58
Vasily Levit <vasily.levit@gmail.com>
2022-10-19 21:33:06 +0800

    add method print

--- first.java
+++ first.java
@@ -2,13 +2,13 @@
  * Написать программу вычисления n-го треугольного числа
  */
 public class first {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         int n = 10;
         int triangleNum = n * (n-1) / 2;
-        print(triangleNum);
+        printRez(triangleNum);
     }
     
-    static void print(int tNum) {
+    static void printRez(int tNum) {
         System.out.println(tNum);
     }
 }
