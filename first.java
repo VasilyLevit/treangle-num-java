2cfde2e54195433ee0958d5ec79c62ee2247a68e
Vasily Levit <vasily.levit@gmail.com>
2022-10-20 19:48:46 +0800

    fixed mistake in equation

--- first.java
+++ first.java
@@ -4,7 +4,7 @@
 public class first {
     public static void main(String[] args) {
         int n = 10;
-        int triangleNum = n * (n-1) / 2;
+        int triangleNum = n * (n+1) / 2;
         printRez(triangleNum);
     }
     
